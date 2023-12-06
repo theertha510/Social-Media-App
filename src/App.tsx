29abfba1929f09facca974f7c046f7333835e379
theertha510 <theerthamk1998@gmail.com>
2023-12-06 09:06:54 +0530

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
+import AuthLayout from './auth/AuthLayout';
+import SigninForm from './auth/forms/SigninForm';
+import RootLayout from './root/RootLayout';
+import { Home } from './root/pages';
+import SignUpForm from './auth/forms/SignUpForm';
+
 function App() {
-  return <p>welcome</p>;
+  return (
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path='/sign-in' element={<SigninForm />} />
+          <Route path='/sign-up' element={<SignUpForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
